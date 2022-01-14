d234a199663eedc7ebf1bf8e4046fe0897f4731b
Girish844 <girish.mishra84420@gmail.com>
2022-01-14 21:51:19 +0530

    add user role 2

--- Hackfest/doctor/views.py
+++ Hackfest/doctor/views.py
@@ -11,67 +11,81 @@ from django.contrib.auth import authenticate
 
 # Create your views here.
 def choiseview(request):
-    return render(request, 'choise.html' )
+    try:
+        return render(request, 'choise.html' )
+    except:
+        return HttpResponse("<h1>something went wrong!!!</h1>")
 
 
 def doctor_register(request, roleid):
-    if request.method =='POST':
-        uname=request.POST.get('uname',None)
-        email=request.POST.get('eml',None)
-        pwd=request.POST.get('pwd',None)
+    try:
+        if request.method =='POST':
+            uname=request.POST.get('uname',None)
+            email=request.POST.get('eml',None)
+            pwd=request.POST.get('pwd',None)
 
-        if User.objects.filter(username=uname).exists():
+            if User.objects.filter(username=uname).exists():
 
-            return HttpResponse("User already available")
-        else:
-            role_data=role.objects.get(role_id= roleid)
-            print(role_data.role)
-            user_obj=User.objects.create(username=uname,password=pwd,email=email)
-            user_obj.set_password(pwd)
-            user_obj.save()
-            roletable= Userrolemap.objects.create(user_id=user_obj, role_id=roleid)
-            roletable.save()
-            print(roleid)
-            messages=role_data.role
-            return render (request, 'dummy.html', {"messages": messages} )
-            
-               
-    return render(request, 'ragister.html')    
+                return HttpResponse("User already available")
+            else:
+                role_data=role.objects.get(role_id= roleid)
+                print(role_data.role)
+                user_obj=User.objects.create(username=uname,password=pwd,email=email)
+                user_obj.set_password(pwd)
+                user_obj.save()
+                roletable= Userrolemap.objects.create(user_id=user_obj, role_id=roleid)
+                roletable.save()
+                messages=role_data.role
+                return render (request, 'dummy.html', {"messages": messages} )
+                
+                
+        return render(request, 'ragister.html')    
+    except:
+        return HttpResponse("<h1>something went wrong!!!</h1>")
 
 def addNurse(request):
-    return  render(request, 'ragister.html',{'roleid': 1 } )
+    try:
+        return  render(request, 'ragister.html',{'roleid': 1 } )
+    except:
+        return HttpResponse("<h1>something went wrong!!!</h1>")
 def addDoctor(request):
-    return  render(request, 'ragister.html',{'roleid': 2} )
+    try:
+        return  render(request, 'ragister.html',{'roleid': 2} )
+    except:
+        return HttpResponse("<h1>something went wrong!!!</h1>")
 
 
-def docter_login(request):    
-    if request.method =='POST':
-        uname=request.POST.get('uname',None)
-        pwd=request.POST.get('pwd',None)
-        ubj= authenticate(request,username=uname,password=pwd)  
-        q = User.objects.filter(username=uname).filter(is_staff=True)
-        if q:
-            print("sonthing maja aagya yaaaaa!!!!!!! ")
-            return redirect("choise/" )
+def docter_login(request):
+    try:    
+        if request.method =='POST':
+            uname=request.POST.get('uname',None)
+            pwd=request.POST.get('pwd',None)
+            ubj= authenticate(request,username=uname,password=pwd)  
+            q = User.objects.filter(username=uname).filter(is_staff=True)
+            if q:
+                print("sonthing maja aagya yaaaaa!!!!!!! ")
+                return redirect("choise/" )
 
 
-     
-        elif ubj:
-            object=Userrolemap.objects.filter(user_id=ubj).filter(role_id=2)
-            if object:
-                messages="username taken thank you!!!"
-                return HttpResponse("hello nurse")
+        
+            elif ubj:
+                object=Userrolemap.objects.filter(user_id=ubj).filter(role_id=2)
+                if object:
+                    messages="username taken thank you!!!"
+                    return HttpResponse("hello nurse")
+                else:
+                    return HttpResponse('Hello Doctor')
+
             else:
-                return HttpResponse('Hello Doctor')
+                messages="Please enter valid details!!!"
+                return render( request, 'index.html', {"messages": messages})
 
         else:
-            messages="Please enter valid details!!!"
-            return render( request, 'index.html', {"messages": messages})
+            messages="Hello, Friend! "
 
-    else:
-        messages="Hello, Friend! "
-
-        return render(request, 'index.html', {"messages": messages})
+            return render(request, 'index.html', {"messages": messages})
+    except:
+        return HttpResponse("<h1>something went wrong!!!</h1>")
 
 
 
