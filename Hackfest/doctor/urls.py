6d21cf8adb8db3c2ac768f3cafc2e1adc1cde5ee
riya1555 <92015180+riya1555@users.noreply.github.com>
2022-01-14 22:29:37 +0530

    Update urls.py

--- Hackfest/doctor/urls.py
+++ Hackfest/doctor/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('' , views.docterLogin),
     path('/logout' , views.doctor_logout),
-    path('patient-list/', views.patientList)
+    path('patient-list/', views.patientList),
     path('patient-list/patient/<int:id>', views.patientRecord),
     path('patient-list/patient/addprescription/', views.addMedicineOnPrescription)
 ]
