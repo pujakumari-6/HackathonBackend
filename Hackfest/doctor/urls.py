ba16cb917d5bf8a8e11a98f421b2a628361f2975
riya1555 <92015180+riya1555@users.noreply.github.com>
2022-01-14 22:28:23 +0530

    Update urls.py

--- Hackfest/doctor/urls.py
+++ Hackfest/doctor/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/loginpg' , views.docter_login),
+    path('' , views.docterLogin),
     path('/logout' , views.doctor_logout),
     path('patient-list/', views.patientList)
+    path('patient-list/patient/<int:id>', views.patientRecord),
+    path('patient-list/patient/addprescription/', views.addMedicineOnPrescription)
 ]
