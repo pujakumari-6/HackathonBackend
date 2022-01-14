bd80adfd69bb42a3ad85bde58236a16387da0dff
Puja Kumari <beingpuja1996@gmail.com>
2022-01-14 15:38:20 +0530

    Implemented the patient registrationby nurse and email sending

--- Hackfest/Hackfest/settings.py
+++ Hackfest/Hackfest/settings.py
@@ -133,3 +133,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'innovativehealthcaregroupk@gmail.com'
+EMAIL_HOST_PASSWORD = 'Group@kProject'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
